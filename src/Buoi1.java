5e5eaa2a612a1a9e7604519177d875bdcd96ffb5
kentechvn <kenny012487@gmail.com>
2022-08-14 17:33:08 +0700

    Initial commit

---
+++ src/Buoi1.java
@@ -0,0 +1,11 @@
+public class Buoi1 {
+    // comment (có dấu // sẽ là comment)
+    // 1: Kiểu dữ liệu
+    // Nhóm 1 (nguyên thủy- primites) chỉ dùng để lưu trữ: char, int, long, double, float, boolean
+    // Nhóm 2 (Đối tượng- Object) dùng để xử lý: String, Integer, Long, Double, Float, Boolean
+    // 2: Khai báo biến tuân thủ theo camelCase
+    char a = 'a'; // char - kiểu ký tự chứa trong ''
+    int age = 28; // int, long - kiểu số nguyên (Số không có phần thập phân)
+    float height = 1.75f; // float, double - kiểu số có phần thập phân
+    boolean isMale = true; // boolean - kiểu luận lý
+}
